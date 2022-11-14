d3ed1f09c5d82259428302ce983263b99df4661d
hunterdog2 <118204818+hunterdog2@users.noreply.github.com>
2022-11-14 11:46:19 -0500

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+input.onButtonPressed(Button.A, function () {
+	
+})
+led.plot(0, 0)
+music.playMelody("- - - - - - - - ", 120)
 radio.setFrequencyBand(0)
 led.plot(0, 0)
 basic.forever(function () {
