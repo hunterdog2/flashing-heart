661eba8f14c674ef805b8b9b3134d98cbce31b44
hunterdog2 <118204818+hunterdog2@users.noreply.github.com>
2022-11-14 11:50:17 -0500

    hunter

--- main.ts
+++ main.ts
@@ -1,10 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-	
+    kitronik_servo_lite.setDegreesPerSecond(0)
 })
-led.plot(0, 0)
-music.playMelody("- - - - - - - - ", 120)
 radio.setFrequencyBand(0)
 led.plot(0, 0)
 basic.forever(function () {
+    kitronik_servo_lite.setDegreesPerSecond(0)
     radio.setTransmitSerialNumber(true)
 })
