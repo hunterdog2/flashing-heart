ad18d0194d7b83ed2cceffd5be01cb65fcfabc64
hunterdog2 <118204818+hunterdog2@users.noreply.github.com>
2022-11-16 11:27:50 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     kitronik_servo_lite.setDegreesPerSecond(0)
+    text_list = ["a", "b", "c"]
 })
+function doSomething (bool: boolean, list: any[], image: Image, text: string, text2: string, text3: string, text4: string, text5: string, text6: string, text7: string) {
+	
+}
+let text_list: string[] = []
 radio.setFrequencyBand(0)
-led.plot(0, 0)
 basic.forever(function () {
     kitronik_servo_lite.setDegreesPerSecond(0)
     radio.setTransmitSerialNumber(true)
+})
+control.inBackground(function () {
+    led.plot(0, 0)
 })
